b1889299f998c9d4edd32df31949dc412ee433fd
RyoSato37 <ryosato.log@gmail.com>
2026-09-14 07:22:34 +0900

    20260914 - プロジェクト初期構成 - 完了

---
+++ src/main/java/com/example/kurashikurabe/KurashikurabeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurashikurabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurashikurabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurashikurabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kurashikurabe/KurashikurabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurashikurabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurashikurabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
